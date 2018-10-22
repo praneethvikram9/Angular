a24cf747adc7fd395ecb71d98311b9abc5b7ad11
CGI-Praneeth <praneethvikram635@gmail.com>
2018-10-22 13:59:53 +0530

    new updates

--- databind-task/src/app/app.module.ts
+++ databind-task/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
-import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CoursesComponent,
-    CourseComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
