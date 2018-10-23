befe83e9eede5915a984e9df5a8e950b261b0e95
CGI-Praneeth <praneethvikram635@gmail.com>
2018-10-23 09:28:52 +0530

    new updates

--- databind-task/src/app/app.module.ts
+++ databind-task/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './scenario1/component1/component1.component';
+import { Component2Component } from './scenario2/component2/component2.component';
+import { Component3Component } from './scenario3/component3/component3.component';
+import { Component4Component } from './scenario3/component4/component4.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component,
+    Component4Component
   ],
   imports: [
     BrowserModule
